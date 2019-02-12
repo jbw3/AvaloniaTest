fdcef1f28423f096d0c47418e06362ff187fb920
John Wilkes <jwilkes0011@gmail.com>
2019-02-11 18:57:19 -0600

    Testing bindings

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,27 @@ namespace AvaloniaMVVMTest.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        private int _index = 0;
+
+        private string[] _greetings = new string[] { "Hello!", "Hi!" };
+
+        private string _Greeting;
+
+        public string Greeting
+        {
+            get => _Greeting;
+            set => this.RaiseAndSetIfChanged(ref _Greeting, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            Greeting = _greetings[0];
+        }
+
+        public void ChangeGreeting()
+        {
+            _index = (_index + 1) % _greetings.Length;
+            Greeting = _greetings[_index];
+        }
     }
 }
